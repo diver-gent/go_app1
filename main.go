1312b486029a121e0713c25d08da4bcaac74dec8
Bill Townsley <bill.townsley@armory.io>
2021-10-05 15:42:44 -0700

    resetting to known state

--- main.go
+++ main.go
@@ -1,32 +1,18 @@
 package main
 
 import (
-        "fmt"
-        "io/ioutil"
-        "log"
-        "net/http"
-
-        "gopkg.in/yaml.v2"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
-const cfgPath = "/config/demo.yaml"
-
-var config struct {
-        Greeting string
-}
-
 func handler(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "%s, 世界\n", config.Greeting)
+	greeting := os.Getenv("GREETING")
+	fmt.Fprintf(w, "%s, 世界\n", greeting)
 }
 
 func main() {
-        cfgData, err := ioutil.ReadFile(cfgPath)
-        if err != nil {
-                log.Fatal(err)
-        }
-        if err = yaml.Unmarshal(cfgData, &config); err != nil {
-                log.Fatalf("failed to parse config file %q: %v", cfgPath, err)
-        }
-        http.HandleFunc("/", handler)
-        log.Fatal(http.ListenAndServe(":7777", nil))
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8888", nil))
 }
