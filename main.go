14fd800a6cced21c5b98efe47f1378181cb7b937
Bill Townsley <bill.townsley@armory.io>
2021-10-05 15:39:55 -0700

    latest changes

--- main.go
+++ main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+        "fmt"
+        "io/ioutil"
+        "log"
+        "net/http"
+
+        "gopkg.in/yaml.v2"
 )
 
+const cfgPath = "/config/demo.yaml"
+
+var config struct {
+        Greeting string
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, App1")
+        fmt.Fprintf(w, "%s, 世界\n", config.Greeting)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":7777", nil))
+        cfgData, err := ioutil.ReadFile(cfgPath)
+        if err != nil {
+                log.Fatal(err)
+        }
+        if err = yaml.Unmarshal(cfgData, &config); err != nil {
+                log.Fatalf("failed to parse config file %q: %v", cfgPath, err)
+        }
+        http.HandleFunc("/", handler)
+        log.Fatal(http.ListenAndServe(":7777", nil))
 }
