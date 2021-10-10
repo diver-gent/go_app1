c837b871ad235c1833e27de008fb805cd0959036
Bill Townsley <bill.townsley@armory.io>
2021-10-10 14:42:23 -0700

    adding service to service comms

--- main.go
+++ main.go
@@ -3,13 +3,29 @@ package main
 import (
 	"fmt"
 	"log"
+	"io/ioutil"
 	"net/http"
-	"os"
+	// "os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	greeting := os.Getenv("GREET")
-	fmt.Fprintf(w, "%s, 世界\n", greeting)
+	// greeting := os.Getenv("GREET")
+	// fmt.Fprintf(w, "%s, 世界\n", greeting)
+	url := "http://app2:7777"
+	resp, err := http.Get(url)
+	if err != nil {
+      fmt.Fprintf(w, "ERROR READING APP2\n")
+	  panic(err)
+	}
+	defer resp.Body.Close()
+	html, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+      fmt.Fprintf(w, "ERROR READING resp.Body\n")
+      panic(err)
+    }
+    // show the HTML code as a string %s
+    fmt.Fprintf(w, "%s\n", html)
+
 }
 
 func main() {
